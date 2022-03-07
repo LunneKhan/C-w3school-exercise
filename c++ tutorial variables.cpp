9e2bc4b6592e79968c42c977480f58e75a9cf84b
Lunne <93430933+LunneKhan@users.noreply.github.com>
2022-03-07 10:56:41 +0100

    Going through variables
    Describing different types of variables in a multi-linecomment

---
+++ c++ tutorial variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+int myNum = 15;
+cout << myNum;
+  return 0;
+}
+/* Now I´m trying out different variables. Variables are containers that store data.
+int - stores integers (whole numbers, no decimals) such as 15 or -10.
+double - stores floating point numbers, with decimals, such as 11,99 or -11,99.
+char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes ('').
+string - stores text, such as "hello world". String values are surrounded by double quotes ("").
+bool - stores values with two states: true or false. */
